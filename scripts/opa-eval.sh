d5ae4a2887531a568b828a0a9047ee7cb50fae9e
Ivan Font <ifont@redhat.com>
2020-03-17 17:48:29 -0700

    Use podman instead of docker

--- scripts/opa-eval.sh
+++ scripts/opa-eval.sh
@@ -3,5 +3,5 @@
 source "$(dirname "${BASH_SOURCE}")/util.sh"
 QUERY_STRING="data.main.deny[message]"
 
-docker run --rm -v ${ROOT_DIR}:/project -w /project openpolicyagent/opa:0.17.3 eval \
+podman run --rm -v ${ROOT_DIR}:/project:Z -w /project openpolicyagent/opa:0.17.3 eval \
   -f pretty --fail-defined -i "${MODEL_ACCURACY_METRICS}" -d "${MODEL_ACCURACY_POLICY}" "${QUERY_STRING}"
