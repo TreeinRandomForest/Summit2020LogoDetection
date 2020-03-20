6d30b21af516c34d51ddecbcdc5778d5c820092d
Ivan Font <ifont@redhat.com>
2020-03-20 15:57:11 -0700

    Add progress logs and extra error handling

--- scripts/ocs.py
+++ scripts/ocs.py
@@ -10,7 +10,8 @@ def get_s3_object(access_key, secret_key, bucket, url, objectName):
                            aws_access_key_id = access_key,
                            aws_secret_access_key = secret_key,
                            verify=False)
-    for key in s3.list_objects(Bucket=bucket)['Contents']:
+    print("Bucket contents:")
+    for key in s3.list_objects(Bucket = bucket)['Contents']:
         print(key['Key'])
         if key['Key'] == objectName:
             return key
@@ -20,7 +21,6 @@ def get_s3_object(access_key, secret_key, bucket, url, objectName):
 def create_json_message(object, s3_bucket_name):
     timestamp = object['LastModified'].timestamp()
     msg = {'modelName': object['Key'], 'timestamp': timestamp, 's3Bucket': s3_bucket_name}
-    print(msg)
     return msg
 
 def send_kafka_message(producer, topic, payload):
@@ -42,13 +42,15 @@ def main():
                                  s3_endpoint_url, model_filename)
 
     if model_object == None:
-        print("Error: S3 object name " + model_filename + " not found")
+        print('Error: S3 object name ' + model_filename + ' not found')
         sys.exit(1)
 
     message = create_json_message(model_object, s3_bucket_name)
+    print('Sending message:', message)
 
-    producer = KafkaProducer(bootstrap_servers=kafka_bootstrap)
+    producer = KafkaProducer(bootstrap_servers = kafka_bootstrap, acks='all')
     send_kafka_message(producer, kafka_topic, json.dumps(message).encode('utf-8'))
+    print('Message sent')
 
 # For more documentation on Producer: https://pypi.org/project/kafka-python/
 if __name__ == '__main__':
