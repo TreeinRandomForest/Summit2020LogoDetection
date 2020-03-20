6f7e19c14460ad93ab19a673c23fa276888a0797
Ivan Font <ifont@redhat.com>
2020-03-20 14:45:36 -0700

    Refactor script for maintainability

--- scripts/ocs.py
+++ scripts/ocs.py
@@ -4,8 +4,31 @@ import os
 import urllib3
 from kafka import KafkaProducer
 
+def get_s3_object(access_key, secret_key, bucket, url, objectName):
+    s3 = boto3.client('s3',endpoint_url = url,
+                           aws_access_key_id = access_key,
+                           aws_secret_access_key = secret_key,
+                           verify=False)
+    for key in s3.list_objects(Bucket=bucket)['Contents']:
+        print(key['Key'])
+        if key['Key'] == objectName:
+            return key
+            break
+    return None
+
+def create_json_message(object, s3_bucket_name):
+    timestamp = object['LastModified'].timestamp()
+    msg = {'modelName': object['Key'], 'timestamp': timestamp, 's3Bucket': s3_bucket_name}
+    print(msg)
+    return msg
+
+def send_kafka_message(producer, topic, payload):
+    producer.send(topic, payload)
+    producer.flush()
+
 def main():
     urllib3.disable_warnings()
+
     s3_access_key = os.environ['ACCESS_KEY_ID']
     s3_secret_key = os.environ['SECRET_ACCESS_KEY']
     s3_bucket_name = os.environ['S3_BUCKET']
@@ -13,20 +36,14 @@ def main():
     model_filename = os.environ['MODEL_FILENAME']
     kafka_bootstrap = os.environ['KAFKA_BOOTSTRAP']
     kafka_topic = os.environ['KAFKA_TOPIC']
-    s3 = boto3.client('s3',endpoint_url= s3_endpoint_url,
-                           aws_access_key_id = s3_access_key,
-                           aws_secret_access_key = s3_secret_key,
-                           verify=False)
-    msg = {}
-    for key in s3.list_objects(Bucket=s3_bucket_name)['Contents']:
-        print(key['Key'])
-        if key['Key'] == model_filename:
-            timestamp = key['LastModified'].timestamp()
-            msg = {'modelName': model_filename, 'timestamp': timestamp, 's3Bucket': s3_bucket_name}
-            break
+
+    model_object = get_s3_object(s3_access_key, s3_secret_key, s3_bucket_name,
+                                 s3_endpoint_url, model_filename)
+
+    message = create_json_message(model_object, s3_bucket_name)
+
     producer = KafkaProducer(bootstrap_servers=kafka_bootstrap)
-    producer.send(kafka_topic, json.dumps(msg).encode('utf-8'))
-    producer.flush()
+    send_kafka_message(producer, kafka_topic, json.dumps(message).encode('utf-8'))
 
 # For more documentation on Producer: https://pypi.org/project/kafka-python/
 if __name__ == '__main__':
