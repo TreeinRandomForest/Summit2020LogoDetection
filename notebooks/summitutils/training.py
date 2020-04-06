f672d862559c21d22a83ea227996dc5c6eaf5b9c
Sophie Watson <sophwats@gmail.com>
2020-04-06 15:48:47 -0500

    removed more prose from notebook

--- notebooks/summitutils/training.py
+++ notebooks/summitutils/training.py
@@ -25,16 +25,15 @@ from detectron2.evaluation import COCOEvaluator, inference_on_dataset
 from detectron2.data import build_detection_train_loader, build_detection_test_loader
 
 def get_dicts(IMG_DIR):
-    '''Returns a list of dicts - one for each image
+    """Returns a list of dicts - one for each image
     Each dict contains labels and bounding boxes
     
     Each folder (train, val, test) contains a data.json file
-    '''
+    """
 
     path = os.path.join(IMG_DIR, 'data.json')
     dataset_dict = json.load(open(path))
     
-    #this is hacky but replace boxmode (add enum encoder/decoder)
     for item in dataset_dict:
         for ann in item['annotations']:
             ann['bbox_mode'] = BoxMode.XYXY_ABS    
@@ -46,26 +45,23 @@ def unregister():
     MetadataCatalog._NAME_TO_META = {}
 
 def register(IMG_DIR, class_names, subfolders=['train', 'test']):
-    '''Register datasets for detectron2
-    '''
+    """Register datasets for detectron2"""
 
     for d in subfolders:
         DatasetCatalog.register(f"{IMG_DIR}_{d}", lambda d=d: get_dicts(f'{IMG_DIR}/{d}'))
         MetadataCatalog.get(f"{IMG_DIR}_{d}").set(thing_classes=class_names)
 
 def get_metadata(dataset_name):
+    
     metadata = MetadataCatalog.get(dataset_name)
 
     return metadata   
 
-def sample_plot(dataset_dict, metadata, LOC):
-    #LOC = 'logos3/train'
+def sample_plot(dataset_dict, metadata, LOC, label_scale=0.5):
 
     d = random.sample(dataset_dict, 1)[0]
-    print(d)
     img = cv2.imread(os.path.join(LOC, d["file_name"]))
-    print(img)
-    visualizer = Visualizer(img, metadata=metadata, scale=0.5)
+    visualizer = Visualizer(img, metadata=metadata, scale=label_scale)
     vis = visualizer.draw_dataset_dict(d)
     plt.imshow(vis.get_image()[:, :, ::-1])
     
@@ -73,27 +69,22 @@ def prepare_for_training(N_iter,
                          output_dir,
                          train_dataset_name,
                          N_classes,
-                         start_training=False,
-                         gpu_avail=True,
-                         model_type="COCO-Detection/faster_rcnn_R_50_C4_1x.yaml"):
+                         start_training=False):
     cfg = get_cfg()
-    cfg.merge_from_file(model_zoo.get_config_file(model_type))
+    cfg.MODEL.DEVICE = 'cpu'
+    cfg.merge_from_file(model_zoo.get_config_file(
+                        "COCO-Detection/faster_rcnn_R_50_C4_1x.yaml"))
     cfg.OUTPUT_DIR = output_dir
     cfg.DATASETS.TRAIN = (train_dataset_name,)
     cfg.DATASETS.TEST = ()
     cfg.DATALOADER.NUM_WORKERS = 2
-    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_type)  # Let training initialize from model zoo
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
+                        "COCO-Detection/faster_rcnn_R_50_C4_1x.yaml")
     cfg.SOLVER.IMS_PER_BATCH = 2
-    cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-    cfg.SOLVER.GAMMA = 0.99 #lr decay
-    cfg.SOLVER.STEPS = list(range(1000, N_iter, 1000)) #(decay steps,)
-    cfg.SOLVER.WARMUP_ITERS = 500 #warmup steps
-    cfg.SOLVER.MAX_ITER = N_iter    # 300 iterations seems good enough for this toy dataset; you may need to train longer for a practical dataset
-    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = N_classes  # 4 classes
-
-    if not gpu_avail:
-        cfg.MODEL.DEVICE = 'cpu'
+    cfg.SOLVER.BASE_LR = 0.00025
+    cfg.SOLVER.MAX_ITER = N_iter
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = N_classes
 
     os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
     trainer = DefaultTrainer(cfg)
@@ -102,7 +93,7 @@ def prepare_for_training(N_iter,
     if start_training:
         trainer.train()
 
-    return trainer, cfg    
+    return trainer, cfg 
 
 def prepare_for_inference(cfg, test_dataset_name, threshold=0.70):
     print(f"Reading weights from output dir: {cfg.OUTPUT_DIR}")
@@ -114,8 +105,8 @@ def prepare_for_inference(cfg, test_dataset_name, threshold=0.70):
     return predictor
 
 def infer_img(predictor, img_filename, metadata, tfm=None):
-    #img = cv2.imread(img_filename)
-    img = Image.open(img_filename)
+    img = cv2.imread(img_filename)
+    #img = Image.open(img_filename)
     if tfm is not None:
         img = tfm(img)
     
